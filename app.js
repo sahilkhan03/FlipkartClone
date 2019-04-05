e78453c1256ccc0a684074ffa9dff9cd8ea6228b
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-04-05 22:32:07 +0530

    Created Index route

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-let app = require('express')();
+let app = require('express')(),
+   http = require('http');
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
 
+
+let routes = require('./routes/index');
 
 
 
 //Listening on port 5000
-app.listen(5000, () => {
+http.listen(5000, () => {
    console.log("Server started on port 5000");
 });
