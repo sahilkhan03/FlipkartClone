f1852998dc3608a94ebe61710c593af7dd689956
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-04-07 22:21:03 +0530

    Installed request-promise package

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let express = require('express'),
+    productsAPI = require('./api/productsAPI'),
+    request = require('request'),
     router = express.Router();
 
 
@@ -8,6 +10,8 @@ router.get('/' , (req,res) => {
    res.render('index');
 });
 
+request('/api/productsAPI')
+    ;
 
 module.exports = router;
 
