22bcbf3959ac3df471288d4079ac97ad95831f13
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-04-08 22:21:48 +0530

    Seeded fake prodcuts

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,13 +1,25 @@
 let faker = require('faker'),
     mongoose = require('mongoose');
 
-mongoose.connect('http://localhost/easeMyCart');
+mongoose.connect('mongodb://localhost/easeMyCart' , { useNewUrlParser: true});
 
 //Require Products Schema
 let Products = require('../models/Products');
 
  for(let i=0;i<100;i++) {
      Products.create({
-
-     })
+         title: faker.commerce.productName(),
+         price: faker.commerce.price(),
+         discount : faker.random.number()%500,
+         image: faker.image.imageUrl(),
+         features : faker.lorem.sentences(),
+         description : faker.lorem.paragraph(),
+         ratings: faker.random.number()%5,
+         brand: faker.company.companyName() ,
+         category: faker.commerce.department()
+     }).then( (product) => {
+         console.log(product);
+     }).catch( (error) => {
+          console.log(error);
+     });
  }
