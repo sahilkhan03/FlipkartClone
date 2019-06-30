5bd4f5bff962d90da09183923de9e93c8c0e6f69
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-06-30 11:06:27 +0530

    Created Route for Homepage

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./homepage/Homepage";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Homepage</h1>
-      </div>
+      <Switch>
+        <Route exact to="/" component={Homepage} />
+      </Switch>
     );
   }
 }
