19748fd686957a7cd001d9ac9caf0ad54bae1899
VIPUL SHARMA <vipulsnp@gmail.com>
2019-07-02 00:15:27 +0530

    Partial changes to Navbar Styling

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <NavLink exact activeStyle={{ color: "red" }} to="/">
-          Home
-        </NavLink>
-        <NavLink exact activeStyle={{ color: "red" }} to="/cart">
-          Cart
-        </NavLink>
-        <NavLink exact activeStyle={{ color: "red" }} to="/login">
-          Login
-        </NavLink>
-        <NavLink exact activeStyle={{ color: "red" }} to="/signup">
-          Signup
-        </NavLink>
-        <NavLink exact activeStyle={{ color: "red" }} to="/about">
-          About
-        </NavLink>
+      <div className="navbar" >
+
+                              {/* Name of Project (on left side)*/}
+        <div className="navbar-brand">
+        <NavLink role="button" className="navbar-item navlinks" exact  to="/"> Flickart    </NavLink>  
+        </div>
+
+                                        {/* SEARCH BAR*/}
+
+              <div id="Search">
+                
+                    <input className="input is-rounded is-hovered" type="text" placeholder="Search"></input>                                      
+              </div>
+
+                                        {/* The Elements to be shown on right side */ }      
+        
+        <div className="navbar-end">
+
+                  <NavLink role="button" className="navbar-item navlinks" exact to="/cart"> 
+                  Cart
+                   </NavLink>
+
+                  <NavLink role="button" className="navlinks navbar-item" exact  to="/login"> Login </NavLink>
+                  
+                  <NavLink role="button" className="navbar-item navlinks" exact  to="/signup"> Signup </NavLink>
+                  
+                  <NavLink role="button" className="navbar-item navlinks" exact  to="/about"> About </NavLink>
+        
+        </div>
+      
       </div>
     );
   }
