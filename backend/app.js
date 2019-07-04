aa0883a15a8ef03b528ed1a5f27a79cb5864b66a
kartik-malik <kartikmalikboss@gmail.com>
2019-07-04 15:47:28 +0530

    added user schema

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ let express = require("express"),
   methodOverride = require("method-override"),
   path = require("path"),
   http = require("http"),
+  User =require("./models/user"),
+  passport  =require("passport"),
+  LocalStrategy=require("passport-local"),
+  methodOverride=require("method-override");
   app = express();
 
 mongoose.connect("mongodb://localhost/easeMyCart", { useNewUrlParser: true });
@@ -15,7 +19,18 @@ app.set("view engine", "ejs");
 app.use(express.static("./"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+//express sesson
+app.use(require("express-session")({
+    secret:"secured",
+    resave:false,
+    saveUninitialized:false
+}));
+//setting passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser())
 //Require all routes here
 let routes = require("./routes");
 let about = require("./routes/about");
